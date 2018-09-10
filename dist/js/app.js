972222083ec53ffd3bf95a1007caec30654569e4
Saad Koubeissi <saad.koubeissi@haymarket.com>
2018-09-10 14:51:22 +0100

    Completed functionality

--- dist/js/app.js
+++ dist/js/app.js
@@ -35,3 +35,62 @@ getVoices();
 if (synth.onvoiceschanged !== undefined) {
   synth.onvoiceschanged = getVoices;
 }
+
+// Speak
+const speak = () => {
+  // Check if speaking
+  if (synth.speaking) {
+    console.error("Already speaking...");
+    return;
+  }
+  if (textInput.value !== "") {
+    // Get speak text
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+    // Speak end
+    speakText.onend = e => {
+      console.log("Done speaking...");
+    };
+
+    // Speak error
+    speakText.onerror = e => {
+      console.error("Oh no something isn't working...");
+    };
+
+    // Selected voice
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
+      "data-name"
+    );
+
+    // Loop through voices
+    voices.forEach(voice => {
+      if (voice.name === selectedVoice) {
+        // speakText is the person speaking
+        speakText.voice = voice;
+      }
+    });
+
+    // Set pitch and rate
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+    // Speak
+    synth.speak(speakText);
+  }
+};
+
+// Event Listeners
+
+// Text form submit
+textForm.addEventListener("submit", e => {
+  e.preventDefault();
+  speak();
+  textInput.blur();
+});
+
+// Rate value change
+rate.addEventListener("change", e => (rateValue.textContent = rate.value));
+
+// Pitch value change
+pitch.addEventListener("change", e => (pitchValue.textContent = pitch.value));
+
+// Voice select change
+voiceSelect.addEventListener("change", e => speak());
